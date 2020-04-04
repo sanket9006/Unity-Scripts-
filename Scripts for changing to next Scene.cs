30c9b460986674c3c50fe11dcab1eaac512a42a2
unknown <sanket.9006@gmail.com>
2020-04-04 18:01:24 +0530

    Scripts

---
+++ Scripts for changing to next Scene.cs
@@ -0,0 +1,11 @@
+﻿ public void Next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
