bb05633e3f52b00c2d8d9536f9c1d26700efa8f2
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-04-30 17:20:07 +0530

    Add files via upload

---
+++ QuitApplication.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuitApplication : MonoBehaviour {
+
+	public void quit() {
+		Debug.log("Application ended");
+		Application.Quit();
+	}
+}
