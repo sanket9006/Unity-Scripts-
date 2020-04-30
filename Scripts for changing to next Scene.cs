1b773f69e2b9243c027920b94ca23f78582b0e13
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2020-04-30 17:21:16 +0530

    Update Scripts for changing to next Scene.cs

--- Scripts for changing to next Scene.cs
+++ Scripts for changing to next Scene.cs
@@ -1,11 +1,107 @@
-﻿ public void Next()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public class change_scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Anatomy_of_Airways()
+    {
+        SceneManager.LoadScene("Anatomy_of_Airways");
     }
 
-    public void Back()
+    public void Menu()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-
+        SceneManager.LoadScene("Menu");
     }
+
+    public void Gastrointestinal_tract()
+    {
+        SceneManager.LoadScene("Gastrointestinal_tract");
+    }
+
+    public void Lymphatic_System()
+    {
+        SceneManager.LoadScene("Lymphatic_System");
+    }
+
+    public void Human_Eukaryotic_Cell()
+    {
+        SceneManager.LoadScene("Human_Eukaryotic_Cell");
+    }
+
+    public void Anatomical_Heart()
+    {
+        SceneManager.LoadScene("Anatomical_Heart");
+    }
+
+    public void Heart_valves_and_auscultation_sites()
+    {
+        SceneManager.LoadScene("Heart_valves_and_auscultation_sites");
+    }
+
+    public void Hydatid_tapeworm()
+    {
+        SceneManager.LoadScene("Hydatid_tapeworm");
+    }
+
+    public void Adult_Echinococcus_Granulosus()
+    {
+        SceneManager.LoadScene("Adult_Echinococcus_Granulosus");
+    }
+
+    public void Adult_E_granulosus_tapeworm()
+    {
+        SceneManager.LoadScene("Adult_E_granulosus_tapeworm");
+    }
+
+
+
+    public void Arm_Anatomy()
+    {
+        SceneManager.LoadScene("Arm_Anatomy");
+    }
+    public void Female_Reproductive_Sy()
+    {
+        SceneManager.LoadScene("Female_Reproductive_Sy");
+    }
+    public void Hemisected_Brain()
+    {
+        SceneManager.LoadScene("Hemisected_Brain");
+    }
+    public void Inner_Ear()
+    {
+        SceneManager.LoadScene("Inner_Ear");
+    }
+
+    public void Craniume()
+    {
+        SceneManager.LoadScene("Craniume");
+    }
+    public void Eyes_Anatomy()
+    {
+        SceneManager.LoadScene("Eyes_Anatomy");
+    }
+    public void Skeleton_Rig()
+    {
+        SceneManager.LoadScene("Skeleton_Rig");
+    }
+
+    public void SignUp()
+    {
+        SceneManager.LoadScene("SignUp");
+    }
+
+    public void SignIn()
+    {
+        SceneManager.LoadScene("SignIn");
+    }
+
+    public void SignIn_SignUp()
+    {
+        SceneManager.LoadScene("SignIn_SignUp");
+    }
+
+
+}
